a4d3f2c085e6545c90e57e6c5a2b1be117a77463
Germán Álvarez <46670724+german-alvarez-dev@users.noreply.github.com>
2023-08-14 15:08:08 +0200

    Initial commit

---
+++ routes/movies.routes.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const router = express.Router()
+
+const Movie = require('./../models/Movie.model')
+const uploaderMiddleware = require('../middleware/uploader.middleware')
+
+
+
+// New movie (render)
+router.get("/crear", (req, res, next) => {
+    res.render("movies/create")
+})
+
+// New movie (handler)
+router.post("/crear", uploaderMiddleware.single('cover'), (req, res, next) => {
+
+    const { title, description } = req.body
+    const { path: cover } = req.file
+
+    Movie
+        .create({ title, description, cover })
+        .then(() => res.redirect('/peliculas'))
+        .catch(err => next(err))
+
+})
+
+// Gallery
+router.get("/", (req, res, next) => {
+
+    Movie
+        .find()
+        .then(movies => res.render('movies/list', { movies }))
+        .catch(err => next(err))
+})
+
+
+
+module.exports = router
